c96d0a3e26443bcd716af27a260c248d353a5687
Kenny Ngo <kenny.ngo.design@gmail.com>
2021-04-01 23:41:04 -0700

    added redux, react-redux, and redux-thunk onto app.js

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text } from 'react-native';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
 
 import PlacesNavigator from './navigation/PlacesNavigator';
+import placesReducer from './store/places-reducer';
+
+// combineReducers - takes an obj that maps the key-value pairs
+const rootReducer = combineReducers({
+  places: placesReducer,
+});
+
+// applyMiddleware is a function that allows the application of ReduxThunk
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 export default function App() {
-  return <PlacesNavigator />;
+  return (
+    <Provider store={store}>
+      <PlacesNavigator />
+    </Provider>
+  );
 }
 
 const styles = StyleSheet.create({});
