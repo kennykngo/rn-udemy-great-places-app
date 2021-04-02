9b87f6315106ab0a768a94860ef3cff3927d2509
Kenny Ngo <kenny.ngo.design@gmail.com>
2021-04-02 16:43:48 -0700

    added FileSystem onto the places-actions.js

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,5 +1,23 @@
+import * as FileSystem from 'expo-file-system';
+
 export const ADD_PLACE = 'ADD_PLACE';
 
 export const addPlace = (title, image) => {
-  return { type: ADD_PLACE, placeData: { title: title, image: image } };
+  return async (dispatch) => {
+    // splits a path string and returns the LAST item, which is the file name (image.png)
+    const fileName = image.split('/').pop();
+    const newPath = FileSystem.documentDirectory + fileName;
+
+    try {
+      await FileSystem.moveAsync({
+        from: image,
+        to: newPath,
+      });
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+
+    dispatch({ type: ADD_PLACE, placeData: { title: title, image: newPath } });
+  };
 };
